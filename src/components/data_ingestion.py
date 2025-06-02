2f953cf0ca3882ad8a9e41f22df998ba22fd0c30
sabiqmohd <sabiqibnshafi@gmail.com>
2025-06-02 23:08:56 +0530

    Data transformation

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,10 +1,13 @@
 import os
 import sys
-from src.logger import logging
-from src.exception import CustomException
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+
+from src.logger import logging
+from src.exception import CustomException
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -38,3 +41,10 @@ class DataIngestion:
 
         except Exception as e:
             raise CustomException(e,sys)
+        
+if __name__=="__main__":
+    obj = DataIngestion()
+    train_data, test_data = obj.initiate_data_ingestion()
+
+    data_transformation = DataTransformation()
+    data_transformation.initiate_data_transformation(train_data, test_data)
--- src/exception.py
+++ src/exception.py
@@ -4,8 +4,8 @@ from src.logger import logging
 def error_message_detail(error, error_details:sys):
     _, _, exc_tb = error_details.exc_info()
     file_name = exc_tb.tb_frame.f_code.co_filename
-    error_message = "Error occured in python scripts name [{0}] line number [{1}] error message [{2}]".format()
-    file_name, exc_tb.tb_lineno, str(error)
+    error_message = "Error occured in python scripts name [{0}] line number [{1}] error message [{2}]".format(
+    file_name, exc_tb.tb_lineno, str(error))
     return error_message
 
 
