9fd8ad8c8f9ba6dbae045946ea500dc3f287634c
sabiqmohd <sabiqibnshafi@gmail.com>
2025-06-03 08:31:51 +0530

    model trainer part added

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import pandas as pd
 import dill
 import os
 import sys
+from sklearn.metrics import r2_score
 
 from src.exception import CustomException
 
@@ -12,5 +13,18 @@ def save_object(file_path, obj):
         os.makedirs(dir_path, exist_ok=True)
         with open(file_path, "wb") as file_obj:
             dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def evaluate_models(X_train, X_test, y_train, y_test, models):
+    try:
+        report = {}
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+            y_pred = model.predict(X_test)
+            model_score = r2_score(y_test, y_pred)
+            report[list(models.keys())[i]] = model_score
+        return report
     except Exception as e:
         raise CustomException(e, sys)
